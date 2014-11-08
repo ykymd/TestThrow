3db4b6389ea0aad093a80bb12fb760b240671f3c
Taichi Tomioka <mys.itachi@gmail.com>
2014-11-08 18:01:36 +0900

    画像読み込み処理を追加。

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ enchant();
 window.onload = function() {
     var game = new Game(320, 480);
     game.fps = 30;
-    game.preload('./img/paper.png');
+    game.preload('./img/paper.png', './img/waste.png',
+                 './img/pause_continue.png', './img/pause_giveup.png',
+                 './img/1-1.png', './img/1-2.png', './img/2-1.png', './img/2-2.png');
     
     game.onload = function() {
         game.replaceScene(createMainScene(game));
