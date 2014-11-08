606ce83da59cfd02d0d740779a68cc4b076c8693
phone <potato_potato_potato@hotmail.co.jp>
2014-11-08 16:40:08 +0900

    画像のディレクトリ変更・メイン画面でのタッチ座標取得

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ enchant();
 window.onload = function() {
     var game = new Game(320, 480);
     game.fps = 30;
-    game.preload('./js/img/paper.png');
+    game.preload('./img/paper.png');
     
     game.onload = function() {
-        game.replaceScene(createSelectScene(game));
+        game.replaceScene(createMainScene(game));
     };
     game.start();
 };
--- js/main_scene.js
+++ js/main_scene.js
@@ -1,13 +1,43 @@
 var createMainScene = function( game ) {
     
+    // initialize
     var scene = new Scene();
-    scene.backgroundColor = '#FFF';
+    scene.backgroundColor = '#00F';
     
     var paper = new Sprite( 200, 300 );
-    paper.image = game.assets['./js/img/paper.png'];
-    paper.x = 60;
-    paper.y = 90;
+    paper.image = game.assets['./img/paper.png'];
+    paper.moveTo(60,90);
+    paper.backgroundColor = "#FFF";
     scene.addChild(paper);
+    
+    var touched = false;
+    var touch = { 'x':0, 'y':0 };
+    
+    var debugLabel = new Label("");
+    debugLabel.moveTo(0,0);
+    debugLabel.color = "#F0F";
+    scene.addChild(debugLabel);
+    
+    // game main
+    
+    scene.addEventListener( Event.TOUCH_START, function(e) {
+        touched = true;
+        touch.x = e.x;
+        touch.y = e.y;
+    } );
+    scene.addEventListener( Event.TOUCH_MOVE, function(e) {
+        touch.x = e.x;
+        touch.y = e.y;
+    } );
+    scene.addEventListener( Event.TOUCH_END, function(e) {
+        touched = false;
+        touch.x = e.x;
+        touch.y = e.y;
+    } );
+    
+    scene.addEventListener( Event.ENTER_FRAME, function(e) {
+        debugLabel.text = Math.floor(touch.x)+","+Math.floor(touch.y);
+    } );
     
     return scene;
 };
