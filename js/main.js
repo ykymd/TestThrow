7ae68078a7c6f6fafb6b2d79ba99178a110532e7
phone <potato_potato_potato@hotmail.co.jp>
2014-11-08 15:13:13 +0900

    画像読み出し部分追加／タブ文字修正

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 enchant();
 
 window.onload = function() {
-	var game = new Game(320, 480);
-	game.fps = 30;
-	game.replaceScene(createTitleScene(game));
-	game.start();
+    var game = new Game(320, 480);
+    game.fps = 30;
+    
+    game.preload('../img/paper.png');
+    
+    game.replaceScene(createTitleScene(game));
+    game.start();
 };
