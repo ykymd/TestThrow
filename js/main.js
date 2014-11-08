6389b23c1b8851a61b5f561bc68729ee7f8094f6
phone <potato_potato_potato@hotmail.co.jp>
2014-11-08 15:53:30 +0900

    画像の表示

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ enchant();
 window.onload = function() {
     var game = new Game(320, 480);
     game.fps = 30;
+    game.preload('./js/img/paper.png');
     
-    game.preload('../img/paper.png');
-    
-    game.replaceScene(createTitleScene(game));
+    game.onload = function() {
+        game.replaceScene(createSelectScene(game));
+    };
     game.start();
 };
--- js/main_scene.js
+++ js/main_scene.js
@@ -1,10 +1,13 @@
-var createMainScene = funtion( Game game ) {
-    var scene = new Scene();
-    var paper = new Sprite( 200, 300 );
-    paper.image = game.assets['./img/paper.png'];
+var createMainScene = function( game ) {
     
+    var scene = new Scene();
+    scene.backgroundColor = '#FFF';
+    
+    var paper = new Sprite( 200, 300 );
+    paper.image = game.assets['./js/img/paper.png'];
+    paper.x = 60;
+    paper.y = 90;
     scene.addChild(paper);
-    scene.backgroundColor = 'rgba( 0, 0, 0, 0 )';
     
     return scene;
-}
+};
