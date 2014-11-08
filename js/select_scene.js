c3326dd0b77e1d8042ab3d5e2898f0e4beaaf00e
Taichi Tomioka <mys.itachi@gmail.com>
2014-11-08 15:43:12 +0900

    スクロールの上限と下限を設定。

--- js/select_scene.js
+++ js/select_scene.js
@@ -3,10 +3,11 @@ var createSelectScene = function(game) {
     var scene = new Scene();
     scene.backgroundColor = '#FFF';
 
+    var numOfStages = 8;
     var buttons = new Group();
     var size = { 'width': 240, 'height': 90 };
     var margin = 30;
-    for (var i = 0; i < 8; i++) {
+    for (var i = 0; i < numOfStages; i++) {
         var sprite = new Sprite(size.width, size.height);
         sprite.backgroundColor = '#0' + i + i;
         sprite.moveTo((game.width - size.width) / 2, i * (size.height + margin) + margin);
@@ -14,6 +15,7 @@ var createSelectScene = function(game) {
     }
     scene.addChild(buttons);
 
+    buttons.height = numOfStages * (size.height + margin) + margin;
     var touchPosition;
     scene.addEventListener(Event.TOUCH_START, function(e) {
                            touchPosition = e;
@@ -22,6 +24,8 @@ var createSelectScene = function(game) {
                            if (touchPosition) {
                            buttons.moveBy(0, e.y - touchPosition.y);
                            touchPosition = e;
+                           if (buttons.y > 0) { buttons.y = 0; }
+                           if (buttons.y < game.height - buttons.height) { buttons.y = game.height - buttons.height; }
                            }
                            });
     scene.addEventListener(Event.TOUCH_END, function(e) {
