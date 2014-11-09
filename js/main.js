efa0e2fd276f748fb048eabdfd481eb64d9c4676
yuki1051 <y.yamada6426@gmail.com>
2014-11-09 09:58:57 +0900

    効果音のラグを修正

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ window.onload = function() {
                  './img/pause.png',
                  './img/pause_continue.png', './img/pause_giveup.png',
                  './img/1-1.png', './img/1-2.png', './img/2-1.png', './img/2-2.png',
-                 './img/3-1.png', './img/3-2.png', './img/4-1.png', './img/4-2.png');
+                 './img/3-1.png', './img/3-2.png', './img/4-1.png', './img/4-2.png',
+                 './sound/sei_ge_gomibako02.mp3');
 
     game.onload = function() {
         game.replaceScene(createTitleScene(game));
