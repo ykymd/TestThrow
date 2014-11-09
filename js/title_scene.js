d2e35d651dc696daed45e2954684c48f6632e0a3
yuki1051 <y.yamada6426@gmail.com>
2014-11-09 11:09:47 +0900

    タイトル画面表示を調整

--- js/title_scene.js
+++ js/title_scene.js
@@ -4,13 +4,13 @@ var createTitleScene = function(game) {
 
     var sound = game.assets[SND_THROW];
     var title_image = game.assets[IMG_TITLE];
-    var sprite = new Sprite(title_image.width/2, title_image.height/2);
+    var sprite = new Sprite(TITLE_IMG_WIDTH, TITLE_IMG_HEIGHT);
     sprite.image = title_image;
     sprite.frame = 0;
     sprite.originX = 0;
     sprite.originY = 0;
-    sprite.scaleX = game.width / title_image.width/2;
-    sprite.scaleY = game.height / title_image.height/2;
+    sprite.scaleX = game.width / TITLE_IMG_WIDTH;
+    sprite.scaleY = game.height / TITLE_IMG_HEIGHT;
     scene.addChild(sprite);
 
     scene.backgroundColor = '#FFF';
