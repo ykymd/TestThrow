4baa6fd3616d1f70b7070ae330cfc9a6ef6c326f
phone <potato_potato_potato@hotmail.co.jp>
2014-11-09 15:15:18 +0900

    リソースのvarをconstに

--- js/resource.js
+++ js/resource.js
@@ -1,30 +1,30 @@
 // リソース一覧
 
 //画像
-var IMG_PAPER = './img/paper.png';  //紙の画像
-var IMG_TRASH = './img/trash.png';  //ゴミ箱の背景画像
+const IMG_PAPER = './img/paper.png';  //紙の画像
+const IMG_TRASH = './img/trash.png';  //ゴミ箱の背景画像
 
-var IMG_TITLE = './img/title.png'; //タイトルの画像
-var BTN_PAUSE = './img/pause.png';
-var BTN_PAUSEMENU = ['./img/pause_continue.png', './img/pause_giveup.png'];
-var SELECT_MENU = './img/1-1.png';
-var IMG_MARK = './img/mark.png';
-var IMG_MARK_CIRCLE = './img/mark_circle.png';
-var IMG_MARK_X = './img/mark_x.png';
-var IMG_GAMESTART = './img/gamestart.png';
-var IMG_GAMEOVER = './img/gameover.png';
+const IMG_TITLE = './img/title.png'; //タイトルの画像
+const BTN_PAUSE = './img/pause.png';
+const BTN_PAUSEMENU = ['./img/pause_continue.png', './img/pause_giveup.png'];
+const SELECT_MENU = './img/1-1.png';
+const IMG_MARK = './img/mark.png';
+const IMG_MARK_CIRCLE = './img/mark_circle.png';
+const IMG_MARK_X = './img/mark_x.png';
+const IMG_GAMESTART = './img/gamestart.png';
+const IMG_GAMEOVER = './img/gameover.png';
 
-var SELECT_BTN_WIDTH = 900;
-var SELECT_BTN_HEIGHT = 400;
-var TITLE_IMG_WIDTH = 1280;
-var TITLE_IMG_HEIGHT = 1960;
-var MARK_IMG_WIDTH = 300;
-var MARK_IMG_HEIGHT = 220;
-var GAMESTART_IMG_WIDTH = 320;
-var GAMESTART_IMG_HEIGHT = 80;
+const SELECT_BTN_WIDTH = 900;
+const SELECT_BTN_HEIGHT = 400;
+const TITLE_IMG_WIDTH = 1280;
+const TITLE_IMG_HEIGHT = 1960;
+const MARK_IMG_WIDTH = 300;
+const MARK_IMG_HEIGHT = 220;
+const GAMESTART_IMG_WIDTH = 320;
+const GAMESTART_IMG_HEIGHT = 80;
 
 //効果音
-var SND_THROW = './sound/sei_ge_gomibako02.mp3';
-var SND_WASTE = './sound/b_sil_paper_marumeru01.mp3';
-var SND_FLY = './sound/sen_ge_byun02.mp3';
-var SND_PASS = './sound/sei_ge_shinbun_basa01.mp3';
+const SND_THROW = './sound/sei_ge_gomibako02.mp3';
+const SND_WASTE = './sound/b_sil_paper_marumeru01.mp3';
+const SND_FLY = './sound/sen_ge_byun02.mp3';
+const SND_PASS = './sound/sei_ge_shinbun_basa01.mp3';
