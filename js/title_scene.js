4baa94ea62424cb5bed17dfbafc8774a26eb6fb6
yuki1051 <y.yamada6426@gmail.com>
2014-11-09 11:05:44 +0900

    タイトル画面変更

--- js/title_scene.js
+++ js/title_scene.js
@@ -3,19 +3,20 @@ var createTitleScene = function(game) {
     var scene = new Scene();
 
     var sound = game.assets[SND_THROW];
-    var title_image = [game.assets[IMG_TITLE[0]],game.assets[IMG_TITLE[1]]];
-    var sprite = new Sprite(title_image[0].width, title_image[0].height);
-    sprite.image = title_image[0];
+    var title_image = game.assets[IMG_TITLE];
+    var sprite = new Sprite(title_image.width/2, title_image.height/2);
+    sprite.image = title_image;
+    sprite.frame = 0;
     sprite.originX = 0;
     sprite.originY = 0;
-    sprite.scaleX = game.width / title_image[0].width;
-    sprite.scaleY = game.height / title_image[0].height;
+    sprite.scaleX = game.width / title_image.width/2;
+    sprite.scaleY = game.height / title_image.height/2;
     scene.addChild(sprite);
 
     scene.backgroundColor = '#FFF';
     scene.addEventListener('touchend', function() {
                            sound.play();
-                           sprite.image = title_image[1];
+                           sprite.frame = 1;
 
                            var curtain = new Sprite(game.width, game.height);
                            curtain.backgroundColor = '#000';
